af16c94332fe6678a16b650586c39aef5c467a9e
leonardo coli <leonardocoli987@gmail.com>
2025-06-23 19:40:35 -0300

    ferias thiago

--- netlify/functions/redirect/redirect.js
+++ netlify/functions/redirect/redirect.js
@@ -25,8 +25,8 @@ exports.handler = async (event, context) => {
       { upsert: true, returnDocument: 'after' }
     );
     const currentIndex = findResult ? findResult.index : 1;
-
-    let whatsappNumber1 = process.env.WHATSAPP_NUMBER_1;
+// whats app 1 thiago ; whats app 2 joice
+    let whatsappNumber1 = process.env.WHATSAPP_NUMBER_2;
     let whatsappNumber2 = process.env.WHATSAPP_NUMBER_2;
 
     if (!whatsappNumber1 || !whatsappNumber2) {
